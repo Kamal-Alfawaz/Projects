8f7e36111bed0bd16f9c62e62ae1d88fa863adaa
Kamal-Alfawaz <95635196+Kamal-Alfawaz@users.noreply.github.com>
2022-08-22 16:13:38 +0100

    Delete MiniProject-Portfolio directory

--- MiniProject-Portfolio/checkIfCanPost.php
+++
@@ -1,8 +0,0 @@
-<?php
-session_start();
-if(($_SESSION['name'])){
-    header('location:BlogPage/BlogPage.html');
-}else{
-    header('location:BlogPage/viewBlog.php');
-}
-?>
--- MiniProject-Portfolio/connection.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-$servername = "127.0.0.1";
-$username = "root";
-$password = "";
-$dbname = "ecs417";
-
-$conn = mysqli_connect($servername, $username, $password, $dbname);
-?>
